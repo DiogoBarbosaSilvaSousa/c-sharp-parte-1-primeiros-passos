63fa0e958606625ae44539210be58b18520dd705
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-09 12:02:08 -0300

    Projeto 2

---
+++ AprendendoCSharp/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variáveis");
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
