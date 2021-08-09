20a80a9741dd8d94824975a3fbabbbaa3c870b98
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-09 10:53:05 -0300

    Criando novo projeto

---
+++ 1-Primeiro Projeto/1-Primeiro Projeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1_Primeiro_Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo. Projeto no Visual Studio!");
+            Console.WriteLine("A execução acabou. Tecle enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
