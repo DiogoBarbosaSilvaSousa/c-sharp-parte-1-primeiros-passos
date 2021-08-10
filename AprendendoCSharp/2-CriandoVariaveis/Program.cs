e2db2ba05884eb3ea728bdcec687b28d6bc5a6d8
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-10 12:16:54 -0300

    Projeto 2

--- AprendendoCSharp/2-CriandoVariaveis/Program.cs
+++ AprendendoCSharp/2-CriandoVariaveis/Program.cs
@@ -7,6 +7,20 @@ namespace _2_CriandoVariaveis
         static void Main(string[] args)
         {
             Console.WriteLine("Executando projeto 2 - Criando Variáveis");
+
+            int idade;
+            idade = 32;
+
+            Console.WriteLine(idade);
+
+            idade = 10;
+
+            Console.WriteLine(idade);
+
+            idade = (10 + 5) * 2;
+
+            Console.WriteLine(idade);
+
             Console.WriteLine("Execução finalizada. Tecle enter para sair...");
             Console.ReadLine();
         }
