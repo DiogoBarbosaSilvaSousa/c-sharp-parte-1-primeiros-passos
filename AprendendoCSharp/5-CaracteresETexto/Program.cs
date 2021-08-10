3496c76345688f9dec245df9d87fa0a520f60c84
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-10 12:17:48 -0300

    Projeto 5

---
+++ AprendendoCSharp/5-CaracteresETexto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+            // character
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de Tecnologia";
+            Console.WriteLine(titulo);
+
+            Console.ReadLine();
+        }
+    }
+}
