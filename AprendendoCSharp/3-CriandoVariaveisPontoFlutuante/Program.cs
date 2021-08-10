4507b8035dc59db0a547badebb262aa73b7d4dc3
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-10 12:17:07 -0300

    Projeto 3

---
+++ AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3 Criando variáveis ponto flutuante");
+
+            double salario;
+            salario = 1200.70;
+
+            Console.WriteLine(salario);
+
+            double idade;
+            idade = 15.0 / 2;
+
+            Console.WriteLine(idade);
+
+            Console.WriteLine("A execução acabou. Tecle enter para sair ...");
+
+            Console.ReadLine();
+        }
+    }
+}
