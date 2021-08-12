9e4e5d38645f29244b489787d50859d4e69f261a
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-12 14:00:56 -0300

    Projeto 6

--- AprendendoCSharp/6-AtribuicoesDeVariaveis/Properties/AssemblyInfo.cs
+++ AprendendoCSharp/6-AtribuicoesDeVariaveis/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("6-AtribuicoesDeVariaveis")]
+[assembly: AssemblyTitle("06-AtribuicoesDeVariaveis")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("6-AtribuicoesDeVariaveis")]
+[assembly: AssemblyProduct("06-AtribuicoesDeVariaveis")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
