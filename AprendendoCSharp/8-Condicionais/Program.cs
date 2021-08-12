03fa4194e1c542d294262c7a8b38b1bd110385bc
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-12 14:01:23 -0300

    Projeto 8

---
+++ AprendendoCSharp/8-Condicionais/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 8 - Condicionais");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+
+            bool acompanhado = quantidadePessoas >= 2;
+
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar.");
+            }
+            else
+            {
+
+                Console.WriteLine("Não pode entrar.");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
