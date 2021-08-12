2139f848428360f25110a439fd553c6836fceca4
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-12 14:01:10 -0300

    Projeto 7

--- AprendendoCSharp/7-Condicionais/Program.cs
+++ AprendendoCSharp/7-Condicionais/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _7_Condicionais
+namespace _07_Condicionais
 {
     class Program
     {
@@ -15,13 +15,13 @@ namespace _7_Condicionais
             int idadeJoao = 16;
             int quantidadePessoas = 2;
 
-            if( idadeJoao >= 18)
+            if (idadeJoao >= 18)
             {
                 Console.WriteLine("João possui mais de 18 anos de idade. Pode entrar.");
             }
             else
             {
-                if( quantidadePessoas >= 2)
+                if (quantidadePessoas >= 2)
                 {
                     Console.WriteLine("João não possui mais de 18 anos, mas está acompanhado.");
                 }
@@ -29,7 +29,7 @@ namespace _7_Condicionais
                 {
                     Console.WriteLine("João não possui mais de 18 anos, não pode entrar.");
                 }
-                
+
             }
 
             Console.ReadLine();
--- AprendendoCSharp/7-Condicionais/Properties/AssemblyInfo.cs
+++ AprendendoCSharp/7-Condicionais/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("7-Condicionais")]
+[assembly: AssemblyTitle("07-Condicionais")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("7-Condicionais")]
+[assembly: AssemblyProduct("07-Condicionais")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
