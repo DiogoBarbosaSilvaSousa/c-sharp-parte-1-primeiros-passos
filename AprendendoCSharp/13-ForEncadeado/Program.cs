23762b9fc6bf41e9a6c83fa044bcfb7fb8becf7e
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-12 13:59:36 -0300

    Projeto 13

---
+++ AprendendoCSharp/13-ForEncadeado/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 13 - For Encadeado");
+
+            for(int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for(int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if(contadorColuna >= contadorLinha)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            // sem usar o break
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+                   
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+ 
